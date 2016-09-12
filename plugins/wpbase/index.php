3d90d436b323e44b8bcc1b5c75dea04b09636d2f
Elias Dorigoni <elias@cabureweb.com>
2016-09-12 15:42:51 -0300

    Metadatos para plugins de theme

---
+++ plugins/wpbase/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WPBase
+Plugin URI: http://eliasdorigoni.com
+Description: Plugin!
+Author: Elias Dorigoni
+Version: 0.1
+Author URI: http://eliasdorigoni.com
+*/
+
+define('WPBASE_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
