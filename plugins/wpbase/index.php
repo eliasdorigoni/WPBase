ea81cd61241c2dc297a00eef9a6445ad3b96fb54
Elias Dorigoni <elias@cabureweb.com>
2016-11-24 18:20:35 -0300

    Galeria agregada

--- plugins/wpbase/index.php
+++ plugins/wpbase/index.php
@@ -11,3 +11,4 @@ Author URI: http://eliasdorigoni.com
 define('WPBASE_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
 
 require_once(WPBASE_PLUGIN_DIR_PATH . 'shortcodes.php');
+require_once(WPBASE_PLUGIN_DIR_PATH . 'galeria/metabox-galeria.php');
