7421018272a685611036d0499a3af595298cb63d
Elias Dorigoni <elias@cabureweb.com>
2017-02-19 12:09:57 -0300

    Elimina imagen de sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,5 @@
 <aside class="sidebar">
-    <img src="http://placehold.it/60x60" />
-
     <?php if (is_active_sidebar('widget-1')): ?>
         <?php dynamic_sidebar('widget-1'); ?>
     <?php endif ?>
-
 </aside>
