1dee4ce93667bb161f9e9e54f0195c29e65ed49e
Elias Dorigoni <elias@cabureweb.com>
2017-02-19 12:00:33 -0300

    Corrige rutas de JS

--- inc/theme_enqueue.php
+++ inc/theme_enqueue.php
@@ -8,8 +8,8 @@ function theme_enqueueCSS() {
 
 function theme_enqueueJS() {
     wp_enqueue_script('jquery');
-    wp_enqueue_script('foundation', ASSETS_JS_URI . '/js/foundation.min.js', array('jquery'), '6.2.3', true );
-    wp_enqueue_script('app', ASSETS_JS_URI.'/js/app.js', array('foundation'), '0.1', true );
+    wp_enqueue_script('foundation', ASSETS_JS_URI . 'vendor/foundation.min.js', array('jquery'), '6.2.3', true );
+    wp_enqueue_script('app', ASSETS_JS_URI.'app.js', array('foundation'), '0.1', true );
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueueCSS');
@@ -19,14 +19,12 @@ add_action('wp_enqueue_scripts', 'theme_enqueueJS');
  * @link https://github.com/kenwheeler/slick/
  */
 function theme_enqueueSlick() {
-    wp_enqueue_style('slick', ASSETS_CSS_URI . '/css/slick.css', array(), '1.6.0');
-    wp_enqueue_script('slick', ASSETS_JS_URI . '/js/vendor/slick.min.js', array('jquery'), '1.6.0', true);
+    wp_enqueue_script('slick', ASSETS_JS_URI . 'vendor/slick.min.js', array('jquery'), '1.6.0', true);
 }
 
 /**
  * @link https://github.com/lokesh/lightbox2/
  */
 function theme_enqueueLightbox() {
-    wp_enqueue_style('lightbox', ASSETS_CSS_URI . '/css/ligtbox.min.css', array(), '2.8.2');
-    wp_enqueue_script('lightbox', ASSETS_JS_URI . '/js/vendor/lightbox.min.js', array('jquery'), '2.8.2', true);
+    wp_enqueue_script('lightbox', ASSETS_JS_URI . 'vendor/lightbox.min.js', array('jquery'), '2.8.2', true);
 }
