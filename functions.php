ca4740b88c3bf28ac57cb69b5b093edcffae128b
Elias Dorigoni <elias@cabureweb.com>
2017-02-23 16:33:27 -0300

    Renombra THEME_ROOT a THEME_DIR

--- functions.php
+++ functions.php
@@ -1,32 +1,33 @@
 <?php
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-define('HOME_URL',        esc_url(home_url()) . '/');
-define('THEME_ROOT',      dirname(__FILE__) . DS);
-define('THEME_URI',       esc_url(get_stylesheet_directory_uri()) . '/');
+define('HOME_URL', esc_url(home_url()) . '/');
+define('THEME_DIR', dirname(__FILE__) . DS);
+define('THEME_URI', esc_url(get_stylesheet_directory_uri()) . '/');
 
+define('ASSETS_DIR',      THEME_DIR . 'assets' . DS);
 define('ASSETS_URI',      THEME_URI  . 'assets/');
-define('ASSETS_DIR',      THEME_ROOT . 'assets/');
+define('ASSETS_CSS_DIR',  ASSETS_DIR . 'css' . DS);
 define('ASSETS_CSS_URI',  ASSETS_URI . 'css/');
+define('ASSETS_JS_DIR',   ASSETS_DIR . 'js' . DS);
 define('ASSETS_JS_URI',   ASSETS_URI . 'js/');
-define('ASSETS_IMG_DIR',  ASSETS_DIR . 'img/');
+define('ASSETS_IMG_DIR',  ASSETS_DIR . 'img' . DS);
 define('ASSETS_IMG_URI',  ASSETS_URI . 'img/');
-define('ASSETS_SVG_DIR',  ASSETS_DIR . 'img/svg/');
-define('ASSETS_SVG_URI',  ASSETS_URI . 'img/svg/');
-
+define('ASSETS_SVG_DIR',  ASSETS_IMG_DIR . 'svg' . DS);
+define('ASSETS_SVG_URI',  ASSETS_IMG_URI . 'svg' . DS);
 define('DISALLOW_FILE_EDIT', true);
 
 if (!isset($content_width)) $content_width = 625;
 
-require THEME_ROOT . 'includes/util.php';
-require THEME_ROOT . 'includes/theme_setup.php';
-require THEME_ROOT . 'includes/theme_widgets.php';
-require THEME_ROOT . 'includes/theme_enqueue.php';
-require THEME_ROOT . 'includes/theme_login.php';
+require THEME_DIR . 'includes/util.php';
+require THEME_DIR . 'includes/theme_setup.php';
+require THEME_DIR . 'includes/theme_widgets.php';
+require THEME_DIR . 'includes/theme_enqueue.php';
+require THEME_DIR . 'includes/theme_login.php';
 
-require THEME_ROOT . 'includes/shortcode.compartir.php';
-require THEME_ROOT . 'includes/shortcode.svg.php';
+require THEME_DIR . 'includes/shortcode.compartir.php';
+require THEME_DIR . 'includes/shortcode.svg.php';
 
-require THEME_ROOT . 'includes/widgets/galeria/class.galeria.php';
+require THEME_DIR . 'includes/widgets/galeria/class.galeria.php';
 
 //////////////
 // Limpieza //
