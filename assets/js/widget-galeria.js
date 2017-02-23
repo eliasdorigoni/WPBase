41f6cd4bad2f332d68b95872d5172800ebe1d1f3
Elias Dorigoni <elias@cabureweb.com>
2017-02-23 17:09:38 -0300

    Agrega widget de galería funcional

--- assets/js/widget-galeria.js
+++ assets/js/widget-galeria.js
@@ -1,77 +1,53 @@
 jQuery(function($) {
-    function actualizarRestantes() {
-        if ($widget.find('.restantes').length > 0)
-            $widget.find('.restantes').html(maximo - cantidadImagenes)
-    }
+    var formatoThumb = '<li class="thumb"><input type="hidden" value="" name="" /><img src="" /> <button type="button" class="button eliminar">Eliminar</button> <button type="button" class="button reemplazar">Reemplazar</button></li>'
 
-    function imagenExiste(id) {
-        var retorno = false
-        $widget.find('input').each(function(k, v) {
-            if (v.value == id)
-                retorno = true
-        })
-        return retorno
-    }
-
-    function eliminarImagen(e) {
+    $('body').on('click', '.widgetGaleriaJS .eliminar', function() {
+        var $widget = $(this).parents('.widgetGaleriaJS')
+        var cantidad = $widget.find('.thumb').length - 1
+        $widget.find('.cantidadActual').html(cantidad)
         $(this).parents('li').remove()
-        cantidadImagenes--
-        actualizarRestantes()
-    }
+    }).on('click', '.widgetGaleriaJS .reemplazar', function(e) {
+        var $thumb = $(this).parents('.thumb')
 
-    function reemplazarImagen(e) {
-        e.preventDefault()
-        var $input = $(this).parents('li')
-        var custom_uploader = wp.media(
-            {
-                title: 'Reemplazar imagen',
-                button: {
-                    text: 'Reemplazar'
-                },
-                multiple: false,
-            }
-        ).on('select', function() {
-            var attachment = custom_uploader.state().get('selection').first().toJSON()
-            if (!imagenExiste(attachment.id)) {
-                $input.find('input').val(attachment.id)
-                $input.find('img').attr('src', attachment.sizes.thumbnail.url)
-            }
+        var uploader = wp.media({
+            title: 'Reemplazar imagen',
+            button: {text: 'Reemplazar'},
+            multiple: false,
         })
-        .open();
-    }
 
-    function agregarImagen(e) {
-        event.preventDefault();
-        var custom_uploader = wp.media({
+        uploader.on('select', function() {
+            var attachment = uploader.state().get('selection').first().toJSON()
+            $thumb.find('input').val(attachment.id)
+            $thumb.find('img').attr('src', attachment.sizes.thumbnail.url)
+        })
+
+        uploader.open()
+    }).on('click', '.widgetGaleriaJS .cargarImagenJS', function(e) {
+        var $widget = $(this).parents('.widgetGaleriaJS')
+        var cantidad = $widget.attr('data-maximo')
+        var limitarCantidad = (cantidad > '0') ? true : false
+
+        var uploader = wp.media({
             title: 'Agregar imágenes a la galería',
-            button: {
-                text: 'Agregar'
-            },
+            button: {text: 'Agregar'},
             multiple: true,
         })
-        .on('select', function() {
-            var models = custom_uploader.state().get('selection').models
+
+        uploader.on('select', function() {
+            var models = uploader.state().get('selection').models
             for(i in models) {
-                var attachment = models[i].toJSON()
-                if (!imagenExiste(attachment.id)) {
-                    var $nuevo = $(li)
-                    $nuevo.find('input').val(attachment.id)
-                    $nuevo.find('img').attr('src', attachment.sizes.thumbnail.url)
-
-                    $widget.find('ul').append($nuevo)
-                    cantidadImagenes++
+                if (limitarCantidad && $widget.find('.thumb').length == cantidad) {
+                    break
                 }
+                var attachment = models[i].toJSON()
+                var $nuevo = $(formatoThumb)
+                $nuevo.find('input').attr('name', $widget.attr('data-name-input')).val(attachment.id)
+                $nuevo.find('img').attr('src', attachment.sizes.thumbnail.url)
+                $widget.find('ul').append($nuevo)
             }
-            actualizarRestantes()
+            $widget.find('.cantidadActual').html($widget.find('.thumb').length)
         })
-        .open();
-    }
 
-    var $widget = $('.widgetGaleriaJS')
-    var cantidadImagenes = $widget.find('input').length
-
-    $widget.on('click', '.eliminar', eliminarImagen)
-             .on('click', '.reemplazar', reemplazarImagen)
-             .on('click', '.cargarImagenJS', agregarImagen)
-
-});
+        uploader.open()
+    })
+})
--- includes/widgets/galeria/class.galeria.php
+++ includes/widgets/galeria/class.galeria.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) exit;
 
 class WP_Widget_Galeria extends WP_Widget
 {
-    public $cantidadMaxima = null;
+    public $cantidadMaxima = 3;
 
     public function __construct()
     {
@@ -18,8 +18,6 @@ class WP_Widget_Galeria extends WP_Widget
         );
         parent::__construct('galeria', 'Galeria', $widget_ops, $control_ops );
         add_action('admin_enqueue_scripts', 'theme_enqueueWidgetGaleria');
-        // Configurar si hace falta limitar la cantidad.
-        // $this->cantidadMaxima = 20;
     }
 
     public function widget($args, $instance)
@@ -27,63 +25,30 @@ class WP_Widget_Galeria extends WP_Widget
         $widget_text = !empty($instance['text']) ? $instance['text'] : '';
         $text = apply_filters('widget_text', $widget_text, $instance, $this);
 
-        echo $args['before_widget']; ?>
-            <div class="textwidget"><?php echo !empty($instance['filter']) ? wpautop($text) : $text; ?></div>
-        <?php
-        echo $args['after_widget'];
+        include 'widget.phtml';
     }
 
     public function update($new_instance, $old_instance)
     {
-        Continuar viendo por que no se actualiza la galeria.
-        // $instance = $old_instance;
-        // $ids = array();
-        // foreach ($instance['galeria'] as $id) {
-        //     if (preg_match('/^\d+$/', $id)) {
-        //         $ids[] = $id;
-        //     }
-        // }
+        $instance = $old_instance;
+        $instance['galeria'] = $new_instance['galeria'];
 
-        // $instance['galeria'] = '';
-        // if ($ids) {
-        //     $instance['galeria'] = implode('|', $ids);
-        // }
-
-        return $new_instance;
+        return $instance;
     }
 
     public function form($instance)
     {
-        $instance = wp_parse_args($instance, array('galeria' => ''));
-        $galeria = array();
+        $instance = wp_parse_args(
+            $instance, 
+            array(
+                'galeria' => array()
+            )
+        );
 
-        if ($instance['galeria']) {
-            $galeria = explode('|', $instance['galeria']);
-        }
-        ?>
-        <div class="widget widgetGaleriaJS media-upload">
-            <?php if ($this->cantidadMaxima): ?>
-                <p><span class="restantes"><?php echo $this->cantidadMaxima - count($galeria); ?></span> imagenes disponibles de <?php echo $this->cantidadMaxima; ?></p>
-            <?php else: ?>
-                <p>Imágenes: <code><pre><?php var_export($instance['galeria']); ?></pre></code></p>
-            <?php endif ?>
-            <button type="button" class="button button-primary cargarImagenJS">Agregar imagenes</button>
-            <?php $formato = '<li class="thumb"><input type="hidden" value="%s" name="galeria[]" /> <img src="%s" /> <button type="button" class="button eliminar">Eliminar</button> <button type="button" class="button reemplazar">Reemplazar</button> </li>'; ?>
-            <ul>
-                <?php
-                foreach($galeria as $id) {
-                    $src = wp_get_attachment_image_src($id);
-                    printf($formato, $id, $src[0]);
-                } 
-                ?>
-            </ul>
-        </div>
-        <script type="text/javascript">
-            var li = '<?php printf($formato, '', '') ?>';
-            var maximo = <?php echo $this->cantidadMaxima ? $this->cantidadMaxima : -1 ?>;
-            var limitarCantidadMaxima = <?php echo is_null($this->cantidadMaxima) ? true : false; ?>
-        </script>
-        <?php
+        $cantidadMaxima = $this->cantidadMaxima;
+        $galeria = ($instance['galeria']) ? $instance['galeria'] : array();
+        
+        include 'form.phtml';
     }
 }
 
