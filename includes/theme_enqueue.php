bc337fe5ed030fda2749881b310165804cc2d4cc
Elias Dorigoni <elias@cabureweb.com>
2017-04-10 17:56:07 -0300

    Une theme_enqueueCSS y theme_enqueueJS

--- includes/theme_enqueue.php
+++ includes/theme_enqueue.php
@@ -1,18 +1,17 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-function theme_enqueueCSS() {
+function theme_enqueue() {
     // wp_enqueue_style('custom-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700', array(), null);
     wp_enqueue_style('app', ASSETS_CSS_URI . 'app.min.css', array(), VERSION);
-}
-add_action('wp_enqueue_scripts', 'theme_enqueueCSS');
 
-function theme_enqueueJS() {
     wp_enqueue_script('jquery');
-    wp_enqueue_script('foundation', ASSETS_JS_URI . 'vendor/foundation.min.js', array('jquery'), '6.2.3', true );
-    wp_enqueue_script('app', ASSETS_JS_URI.'app.js', array('foundation'), VERSION, true );
+    wp_enqueue_script('foundation', ASSETS_JS_URI . 'vendor/foundation.min.js', array('jquery'), '6.2.3', true);
+    wp_enqueue_script('extend-nav', ASSETS_JS_URI . 'extend/navegacion-responsive.js', array('jquery'), VERSION, true);
+    wp_enqueue_script('nav', ASSETS_JS_URI.'vendor/superfish.min.js', array('jquery'), '1.7.9', true);
+    wp_enqueue_script('app', ASSETS_JS_URI.'app.js', array('foundation'), VERSION, true);
 }
-add_action('wp_enqueue_scripts', 'theme_enqueueJS');
+add_action('wp_enqueue_scripts', 'theme_enqueue');
 
 function theme_enqueueBackend() {
     wp_enqueue_style('backend', ASSETS_CSS_URI . 'backend.min.css', array(), VERSION);
