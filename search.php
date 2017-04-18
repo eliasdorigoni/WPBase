10ada2b3216ef3035146e994863ec6f0df27c7c1
Elias Dorigoni <elias@cabureweb.com>
2017-04-17 22:21:36 -0300

    Elimina templates de contenido

--- search.php
+++ search.php
@@ -11,8 +11,30 @@
             <?php if (have_posts()): ?>
                 <?php while (have_posts()): the_post(); ?>
 
-                <?php get_template_part('templates/contenido', 'busqueda'); ?>
+                    <article <?php post_class(); ?>>
+                        <header class="header-entrada">
+                            <h2 class="titulo-entrada"><a href="<?php esc_url(get_permalink()); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                        </header>
 
+                        <?php if (has_post_thumbnail()): ?>
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        <?php endif ?>
+
+                        <?php the_excerpt(); ?>
+
+                        <footer class="footer-entrada">
+                            <?php
+                                edit_post_link(
+                                    sprintf(
+                                        'Editar <span class="screen-reader-text">"%s"</span>',
+                                        get_the_title()
+                                        ),
+                                    '<span class="edit-link">',
+                                    '</span>'
+                                    );
+                            ?>
+                        </footer>
+                    </article><!-- #post-## -->
                 <?php endwhile ?>
 
             <?php the_posts_pagination(array(
@@ -23,7 +45,7 @@
 
         <?php else: ?>
 
-            <?php get_template_part( 'templates/contenido', 'vacio' ); ?>
+            <p>Sin resultados.</p>
 
         <?php endif; ?>
 
