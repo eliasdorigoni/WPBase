600f62acf4127f54eb0aa37f3cc517353ce0a568
Elias Dorigoni <elias@cabureweb.com>
2017-05-21 10:37:05 -0300

    Usa las redes definidas en la clase

--- includes/redes-sociales/class.redes-sociales.php
+++ includes/redes-sociales/class.redes-sociales.php
@@ -3,42 +3,70 @@ if (!defined('ABSPATH')) exit;
 
 class RedesSociales
 {
+    static $config = array(
+        'twitter' => array(
+            'nombre' => 'Twitter',
+            'option' => 'url-twitter',
+            'customizer' => array(
+                'label' => 'URL de Twitter',
+            ),
+        ),
+        'facebook' => array(
+            'nombre' => 'Facebook',
+            'option' => 'url-facebook',
+            'customizer' => array(
+                'label' => 'URL de Facebook',
+            ),
+        ),
+        'google-plus' => array(
+            'nombre' => 'Google Plus',
+            'option' => 'url-google-plus',
+            'customizer' => array(
+                'label' => 'URL de Google Plus',
+            ),
+        ),
+        'youtube' => array(
+            'nombre' => 'YouTube',
+            'option' => 'url-youtube',
+            'customizer' => array(
+                'label' => 'URL del canal de Youtube',
+            ),
+        ),
+        'instagram' => array(
+            'nombre' => 'Instagram',
+            'option' => 'url-instagram',
+            'customizer' => array(
+                'label' => 'URL de Instagram',
+            ),
+        ),
+    );
+
     static function registrarOpcionesCustomizer($wp_customize)
     {
         $wp_customize->add_section('redes-sociales', array(
             'title' => 'Redes sociales',
             'description' => 'Agrega las direcciones de sus redes sociales. Déjelas vacías para no usarlas.',
-            'panel' => '', // Not typically needed.
             'priority' => 160,
             'capability' => 'edit_theme_options',
-            'theme_supports' => '', // Rarely needed.
-            ));
+        ));
 
-        $redesSociales = array(
-            'url-twitter'     => 'URL de Twitter',
-            'url-facebook'    => 'URL de Facebook',
-            'url-google-plus' => 'URL de Google Plus',
-            'url-youtube'     => 'URL del canal de Youtube',
-            'url-instagram'   => 'URL de Instagram',
-            );
-
-        foreach ($redesSociales as $setting => $label) {
-            $wp_customize->add_setting($setting, array(
+        foreach (self::$config as $slug => $item) {
+            $wp_customize->add_setting($item['option'], array(
                 'type' => 'option',
                 'capability' => 'edit_theme_options',
-                ));
+            ));
             $wp_customize->add_control(
                 new WP_Customize_Control(
                     $wp_customize,
-                    $setting . '-control',
+                    $item['option'] . '-control',
                     array(
-                        'label'         => $label,
-                        'section'       => 'redes-sociales',
-                        'settings'      => $setting,
-                        'type'          => 'text',
-                        )
+                        'label' => $item['customizer']['label'],
+                        'section' => 'redes-sociales',
+                        'settings' => $item['option'],
+                        'type' => 'text',
                     )
-                );
+                )
+            );
         }
     }
 }
