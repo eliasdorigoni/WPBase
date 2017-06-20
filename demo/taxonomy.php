8c69c7573ecef2815e88778f5c6396c8d2bcf11f
Elias Dorigoni <elias@cabureweb.com>
2017-06-20 07:53:09 -0300

    Agrega taxonomia demo

---
+++ demo/taxonomy.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+function registrarTaxonomiaCustom() {
+    register_taxonomy('operacion', array('cosa'), array(
+        'hierarchical'      => true,
+        'labels'            => construirLabels('operacion', 'operacion', 'operaciones', false),
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true
+    ));
+    register_taxonomy_for_object_type('operacion', 'cosa');
+}
+add_action('init', 'registrarTaxonomiaCustom');
