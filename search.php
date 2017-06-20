cc021c7126d47968aae7352d909959822a1169a6
Elias Dorigoni <elias@cabureweb.com>
2017-06-20 08:15:38 -0300

    Normaliza clases en search.php

--- search.php
+++ search.php
@@ -2,13 +2,11 @@
 
 <div class="row">
     <div class="column large-8">
-        <main role="main" class="main">
-
-            <header class="header-main">
-                <h1 class="titulo-pagina">Resultados de búsqueda de: <?php echo get_search_query(); ?></h1>
-            </header>
+        <main id="contenido" role="main" class="main">
 
             <?php if (have_posts()): ?>
+                <h1 class="tituloPagina">Resultados de búsqueda de: <?php echo get_search_query(); ?></h1>
+
                 <?php while (have_posts()): the_post(); ?>
 
                     <article <?php post_class(); ?>>
@@ -37,17 +35,16 @@
                     </article><!-- #post-## -->
                 <?php endwhile ?>
 
-            <?php the_posts_pagination(array(
-                'prev_text'          => '&lt; Anterior',
-                'next_text'          => 'Siguiente &gt;',
-                'before_page_number' => '<span class="meta-nav screen-reader-text">Pagina</span> ',
-                )); ?>
+                <?php the_posts_pagination(array(
+                    'prev_text'          => '&lt; Anterior',
+                    'next_text'          => 'Siguiente &gt;',
+                    'before_page_number' => '<span class="meta-nav screen-reader-text">Pagina</span> ',
+                    )); ?>
 
-        <?php else: ?>
-
-            <p>Sin resultados.</p>
-
-        <?php endif; ?>
+            <?php else: ?>
+                <h1 class="tituloPagina">No hay resultados</h1>
+                <p>No se encontradon coincidencias para: <?php echo get_search_query(); ?></p>
+            <?php endif; ?>
 
         </main>
     </div>
