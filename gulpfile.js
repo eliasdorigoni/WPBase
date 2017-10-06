aca5ed6a5cb066d4becff830bb96146b72898780
Elias Dorigoni <elias@cabureweb.com>
2017-10-06 10:47:25 -0300

    Modifica tarea default para permitir watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,31 @@
 // @TODO: permitir minificar SVGs de la raiz de source/svg/, y los que esten en carpetas convertirlos en sprites agrupados.
 
 var gulp         = require('gulp'),
+    argv         = require('yargs').argv,
     runSequence  = require('run-sequence'),
     requireDir   = require('require-dir'),
     CONFIG       = require('./gulp/config.js')
 
 requireDir('./gulp/tasks');
 
-gulp.task('default', [
-    'phplint',
-    'js',
-    'sass',
-    'comprimir-imagenes',
-    'svg',
-    'includes-js',
-    'favicon',
-    'extraer-source',
-    'copiar-plugins',
-])
+gulp.task('default', function() {
+    var secuencia = [
+        'phplint',
+        'js',
+        'sass',
+        'comprimir-imagenes',
+        'svg',
+        'includes-js',
+        'favicon',
+        'extraer-source',
+        'copiar-plugins',
+    ]
+    if (argv.watch) {
+        runSequence(secuencia, 'watch')
+    } else {
+        runSequence(secuencia)
+    }
+})
 
 gulp.task('init', function() {
     runSequence('clean-assets', 'default');
