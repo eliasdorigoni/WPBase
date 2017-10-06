3ef6bba8775a63c042f21419c5b0fcada70f316c
Elias Dorigoni <elias@cabureweb.com>
2017-10-06 10:43:11 -0300

    Prepara las tareas principales

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,29 @@
 // @TODO: permitir minificar SVGs de la raiz de source/svg/, y los que esten en carpetas convertirlos en sprites agrupados.
 
 var gulp         = require('gulp'),
-    argv         = require('yargs').argv,
-    autoprefixer = require('gulp-autoprefixer'),
-    concat       = require('gulp-concat'),
-    del          = require('del'),
-    favicons     = require('gulp-favicons'),
-    gulpIgnore   = require('gulp-ignore'),
-    gulpif       = require('gulp-if'),
-    gutil        = require('gulp-util'),
-    imagemin     = require('gulp-imagemin'),
-    livereload   = require('gulp-livereload'),
-    newer        = require('gulp-newer'),
-    phplint      = require('gulp-phplint'),
-    rename       = require("gulp-rename"),
     runSequence  = require('run-sequence'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    svgSprite    = require('gulp-svg-sprite'),
-    uglify       = require('gulp-uglify'),
     requireDir   = require('require-dir'),
     CONFIG       = require('./gulp/config.js')
 
 requireDir('./gulp/tasks');
 
-gulp.task('default', function() {
-    runSequence(['foundation-js', 'favicon', 'phplint'], [
-            'extraer-source',
-            // 'copiar-plugins',
-            'completar-build',
-            'comprimir-screenshot',
-            'sass',
-            'js',
-            'comprimir-imagenes',
-            'svg',
-            'svg-sprite',
-        ]
-    );
-})
+gulp.task('default', [
+    'phplint',
+    'js',
+    'sass',
+    'comprimir-imagenes',
+    'svg',
+    'includes-js',
+    'favicon',
+    'extraer-source',
+    'copiar-plugins',
+])
 
 gulp.task('init', function() {
-    runSequence('default', 'watch')
+    runSequence('clean-assets', 'default');
 })
 
 gulp.task('build', function() {
     CONFIG.esBuild(true)
-    runSequence('clean', 'default')
+    runSequence('clean-build', ['default', 'comprimir-screenshot', 'completar-build'])
 })
