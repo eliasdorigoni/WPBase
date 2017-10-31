0949af3ff29121f219867d08183cd29f1de07c87
Elias Dorigoni <elias@cabureweb.com>
2017-10-31 07:15:00 -0300

    Actualiza funcion de fechas relativas

--- includes/util.php
+++ includes/util.php
@@ -1,77 +1,52 @@
 <?php
 /**
- * Calcula la diferencia entre dos timestamps y retorna un array con las
- * unidades temporales más altas, en orden descendente (año, mes, semana, dia. etc).
+ * Calcula la diferencia entre dos timestamps y retorna un string con las
+ * unidades temporales más altas, en orden descendente: año, mes, semana, dia, hora, minuto, segundo.
  * Sirve para calcular las fechas de "hace x minutos" o "en x semanas".
- * Maneja automáticamente los singulares y plurales.
  * 
- * @param  integer $timestamp Tiempo a comparar, en el pasado o futuro
- * @param  integer $unidades  Unidades temporales a mostrar. 
- *                            Ej: 1 puede devolver "hora", 2 puede devolver
- *                            "semanas" y "dias".
- * @param  integer $comparar  Fecha a comparar. Por defecto, time().
- * @return array              Array de 2 o más valores.
- *                            El primero es un booleano que indica si el tiempo está
- *                            en el futuro. El resto son las unidades temporales.
+ * @param  integer $timestamp             Tiempo a comparar, en el pasado o futuro
+ * @param  integer $timestampComparativo  Fecha a comparar. Por defecto, time().
+ * @param  integer $retornarCantidadItems Cuántos "tiempos" devolver en el string. Por defecto 2.
+ * @return string
  */
-function fechaTextual($timestamp = 0, $unidades = 2, $comparar = 0) {
-    if (!is_numeric($timestamp)) return array();
-    if (!$comparar) $comparar = time();
-    $diferencia = $comparar - $timestamp;
-    $fechaEsFutura = ($diferencia < 0) ? true : false;
+function tiempoRelativo($timestamp, $timestampComparativo = null, $retornarCantidadItems = 2) {
+    $retorno = array();
+    if (is_null($timestampComparativo)) {
+        $timestampComparativo = time();
+    }
 
-    $valores = array(
-        'año'     => 0,
-        'mes'     => 0,
-        'semana'  => 0,
-        'dia'     => 0,
-        'hora'    => 0,
-        'minuto'  => 0,
-        'segundo' => 0,
-        );
+    $diferenciaTotal = $timestamp - $timestampComparativo;
+    $prefijo = ($diferenciaTotal > 0) ? 'en ' : 'hace ';
 
-    $constantes = array(
-        'año'     => YEAR_IN_SECONDS,
-        'mes'     => MONTH_IN_SECONDS,
-        'semana'  => WEEK_IN_SECONDS,
-        'dia'     => DAY_IN_SECONDS,
-        'hora'    => HOUR_IN_SECONDS,
-        'minuto'  => MINUTE_IN_SECONDS,
-        'segundo' => 1
-        );
+    $diferenciaAbsoluta = abs($diferenciaTotal);
 
-    foreach ($constantes as $k => $constante) {
-        if ($diferencia > $constante) {
-            $valores[$k] = floor($diferencia / $constante);
-            $diferencia = $diferencia % $constante;
+    $limites = array(
+        array(YEAR_IN_SECONDS, 'año', 'años'),
+        array(MONTH_IN_SECONDS, 'mes', 'meses'),
+        array(WEEK_IN_SECONDS, 'semana', 'semanas'),
+        array(DAY_IN_SECONDS, 'dia', 'dias'),
+        array(HOUR_IN_SECONDS, 'hora', 'horas'),
+        array(MINUTE_IN_SECONDS, 'minuto', 'minutos'),
+        array(1, 'segundo', 'segundos'),
+    );
+
+    foreach ($limites as $limite) {
+        if ($diferenciaAbsoluta >= $limite[0]) {
+            $diferencia = round($diferenciaAbsoluta / $limite[0]);
+            $texto = ($diferencia == 1) ? $limite[1] : $limite[2];
+            $retorno[] = $diferencia . ' ' . $texto;
+
+            $diferenciaAbsoluta = $diferenciaAbsoluta % $limite[0];
         }
     }
 
-    $retorno = array($fechaEsFutura);
-
-    $plural = array(
-        'año'     => 'años',
-        'mes'     => 'meses',
-        'semana'  => 'semanas',
-        'dia'     => 'dias',
-        'hora'    => 'horas',
-        'minuto'  => 'minutos',
-        'segundo' => 'segundos'
-        );
-
-    while ($unidades > 0) {
-        foreach ($valores as $k => $v) {
-            if ($v != 0) {
-                $retorno[] = $v . ' ' . plural($v, $k, $plural[$k]);
-                unset($valores[$k]);
-                break;
-            }
-            unset($valores[$k]);
-        }
-        $unidades--;
+    if (count($retorno) === 0) {
+        $retorno[] = 0 . ' ' . $limite[2];
+    } else if ($retornarCantidadItems !== -1 && count($retorno) > $retornarCantidadItems) {
+        $retorno = array_slice($retorno, 0, $retornarCantidadItems);
     }
 
-    return $retorno;
+    return $prefijo . implode(', ', $retorno);
 }
 
 function fechaEsCorrecta($fecha = 0, $formato = 'Y-m-d H:i:s') {
