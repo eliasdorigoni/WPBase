a4c3b600a3e850c8ad17518e1b223b81b203c4a2
Elias Dorigoni <elias@cabureweb.com>
2017-11-12 10:57:06 -0300

    Reescribe la configuracion

--- gulp/config.js
+++ gulp/config.js
@@ -1,173 +1,110 @@
 var argv = require('yargs').argv,
     path = require('path')
 
-function armarDependenciasFoundation(plugins) {
-    if (typeof plugins !== 'object' || plugins.length == 0) {
-        return '';
+/**
+ * Recibe un array de plugins y devuelve las rutas de los mismos con las dependencias requeridas.
+ * @param  array    pluginsRequeridos  Plugins a usar
+ * @return array                       Todos los scripts a procesar, o un array vacío.
+ */
+function armarDependenciasFoundation(pluginsRequeridos) {
+    if (typeof pluginsRequeridos !== 'object' || pluginsRequeridos.length == 0) {
+        return [];
     }
 
-    var dependenciasRequeridas = [
-        'core',
-        'util.mediaQuery',
-    ]
-
-    var utilidadesDisponibles = [
-        'util.box',
-        'util.keyboard',
-        'util.imageLoader',
-        'util.motion',
-        'util.nest',
-        'util.timer',
-        'util.timerAndImageLoader',
-        'util.touch',
-        'util.triggers',
-    ]
-
-    var pluginsDisponibles = {
-        'abide': [
-        ],
-        'accordion': [
-            'util.keyboard'
-        ],
-        'accordionMenu': [
-            'util.keyboard',
-            'util.nest',
-        ],
-        'drilldown': [
-            'util.keyboard',
-            'util.nest',
-            'util.box',
-        ],
-        'dropdown': [
-            'util.keyboard',
-            'util.box',
-            'util.triggers',
-        ],
-        'dropdownMenu': [
-            'util.keyboard',
-            'util.box',
-            'util.nest',
-        ],
-        'equalizer': [
+    var dependenciasObligatorias = [
+            'core',
             'util.mediaQuery',
-            'util.imageLoader', // Usar si tiene imagenes.
         ],
-        'interchange': [
-            'util.mediaQuery'
-        ],
-        'magellan': [
-            'smoothScroll'
-        ],
-        'offcanvas': [
+        utilidadesDisponibles = [
+            'util.box',
             'util.keyboard',
-            'util.mediaQuery',
-            'util.triggers',
-        ],
-        'orbit': [
-            'util.keyboard',
+            'util.imageLoader',
             'util.motion',
+            'util.nest',
+            'util.timer',
             'util.timerAndImageLoader',
             'util.touch',
-        ],
-        'responsiveAccordionTabs': [
-            'util.motion',
-            'accordion',
-            'tabs',
-        ],
-        'responsiveMenu': [
-            'util.triggers',
-            'util.mediaQuery',
-        ],
-        'responsiveToggle': [
-            'util.mediaQuery',
-            'util.motion',
-        ],
-        'reveal': [
-            'util.keyboard',
-            'util.triggers',
-            'util.mediaQuery',
-            'util.motion', // Usar si hay animaciones
-        ],
-        'slider': [
-            'util.motion',
-            'util.triggers',
-            'util.keyboard',
-            'util.touch',
-        ],
-        'smoothScroll': [
-        ],
-        'sticky': [
-            'util.triggers',
-            'util.mediaQuery',
-        ],
-        'tabs': [
-            'util.keyboard',
-            'util.imageLoader', // Usar si tiene imagenes
-        ],
-        'toggler': [
-            'util.motion',
             'util.triggers',
         ],
-        'tooltip': [
-            'util.box',
-            'util.mediaQuery',
-            'util.triggers',
-        ],
-    }
-
-    var utilidadesUsadas = [],
+        pluginsDisponibles = {
+            'abide': [],
+            'accordion': ['util.keyboard'],
+            'accordionMenu': ['util.keyboard', 'util.nest'],
+            'drilldown': ['util.keyboard', 'util.nest', 'util.box'],
+            'dropdown': ['util.keyboard', 'util.box', 'util.triggers'],
+            'dropdownMenu': ['util.keyboard', 'util.box', 'util.nest'],
+            // Usar util.imageLoader si el script maneja imagenes
+            'equalizer': ['util.mediaQuery', 'util.imageLoader'],
+            'interchange': ['util.mediaQuery'],
+            'magellan': ['smoothScroll'],
+            'offcanvas': ['util.keyboard', 'util.mediaQuery', 'util.triggers'],
+            'orbit': ['util.keyboard', 'util.motion', 'util.timerAndImageLoader', 'util.touch'],
+            'responsiveAccordionTabs': ['util.motion', 'accordion', 'tabs'],
+            'responsiveMenu': ['util.triggers', 'util.mediaQuery'],
+            'responsiveToggle': ['util.mediaQuery', 'util.motion'],
+            // Usar util.motion si el script usa animaciones
+            'reveal': ['util.keyboard', 'util.triggers', 'util.mediaQuery', 'util.motion'],
+            'slider': ['util.motion', 'util.triggers', 'util.keyboard', 'util.touch'],
+            'smoothScroll': [],
+            'sticky': ['util.triggers', 'util.mediaQuery'],
+            // Usar util.imageLoader si el script maneja imagenes
+            'tabs': ['util.keyboard', 'util.imageLoader'],
+            'toggler': ['util.motion', 'util.triggers'],
+            'tooltip': ['util.box', 'util.mediaQuery', 'util.triggers'],
+        },
+        utilidadesRequeridas = [],
         pluginsUsados = []
 
-    for (i in pluginsDisponibles) {
-        if (plugins.indexOf(i) === -1) {
-            continue;
-        }
+    for (i in pluginsRequeridos) {
+        var plugin = pluginsRequeridos[i]
+        pluginsUsados.push(plugin)
 
-        for (j in pluginsDisponibles[i]) {
-            var plugin = pluginsDisponibles[i][j]
-            if (utilidadesUsadas.indexOf(plugin) === -1 && dependenciasRequeridas.indexOf(plugin) === -1) {
-                utilidadesUsadas.push(plugin)
+        for (j in pluginsDisponibles[plugin]) {
+            var dependencia = pluginsDisponibles[plugin][j]
+
+            // Agrega el plugin si no fue agregado antes.
+            if (utilidadesRequeridas.indexOf(dependencia) === -1 
+                && dependenciasObligatorias.indexOf(dependencia) === -1) {
+                utilidadesRequeridas.push(dependencia)
             }
         }
-        pluginsUsados.push(i)
     }
 
-    if (utilidadesUsadas.length > 0) {
-        var scripts = dependenciasRequeridas.concat(utilidadesUsadas, pluginsUsados)
-        for (i in scripts) {
-            scripts[i] = './node_modules/foundation-sites/dist/js/plugins/foundation.' + scripts[i] + '.js'
-        }
-        return scripts
+    if (utilidadesRequeridas.length == 0) {
+        return []
     }
-    return []
+
+    var scripts = dependenciasObligatorias.concat(utilidadesRequeridas, pluginsUsados).map(function(nombre) {
+        return './node_modules/foundation-sites/dist/js/plugins/foundation.' + nombre + '.js'
+    })
+
+    return scripts
 }
+
+function esBuild() {
+    return typeof argv.build === 'boolean'
+}
+
+function definirRoot() {
+    var retorno = './'
+
+    if (esBuild()) {
+        var nombreTheme = path.resolve('./').split('\\').pop()
+        retorno += 'build/' + nombreTheme + '/'
+    }
+
+    return retorno
+}
+
+var themeRoot = definirRoot()
+
 
 module.exports = {
-    dir: {
-        root: './',
-        assets: './assets/',
-        theme: path.resolve('./') + '\\',
-    },
-    esBuild: function(bool) {
-        if (typeof bool === 'boolean') {
-            // bool esta configurado
-        } else if (typeof argv.build === 'boolean') {
-            // Si --build esta definido, es boolean (true)
-            bool = argv.build
-        } else {
-            bool = false
-        }
-
-        if (bool === true) {
-            this.dir.root = './build/' + path.resolve('./').split('\\').pop() + '/'
-        } else {
-            this.dir.root = './'
-        }
-        this.dir.assets = this.dir.root + 'assets/'
-
-        return bool
-    },
+    esBuild: esBuild(),
+    noEsBuild: !esBuild(),
     dependenciasJS: armarDependenciasFoundation(['equalizer', 'offcanvas', 'reveal', 'toggler']),
+    dir: {
+        root: themeRoot,
+        assets: themeRoot + 'assets/',
+    },
 }
-
-module.exports.esBuild() // Corrige el directorio si existe la bandera --build
