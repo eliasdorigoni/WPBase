3f126358462e35a0ba4f1e2984fe690a43fbb0af
Elias Dorigoni <elias@cabureweb.com>
2017-12-10 09:46:10 -0300

    Elimina widget de galeria, ya existe uno en WP

--- includes/galeria/init.php
+++ includes/galeria/init.php
@@ -3,12 +3,10 @@ if (!defined('ABSPATH')) exit;
 
 require_once 'metabox.php';
 require_once 'shortcode.php';
-require_once 'widget.php';
 
 add_action('add_meta_boxes', 'registrarMetaboxGaleria');
 add_action('save_post', 'guardarMetaboxGaleria', 1, 1);
 add_shortcode('galeria', 'mostrarGaleria');
-add_action('widgets_init', 'registrarWidgetGaleria');
 
 function theme_enqueueGaleriaBackend() {
     wp_enqueue_script('backend', THEME_URI . 'includes/galeria/backend-galeria.js', array('jquery'), VERSION_THEME, true);
