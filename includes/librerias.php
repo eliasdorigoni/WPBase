7f8d4f2c28e1a5eb848881f6d94eaf0c32357503
Elias Dorigoni <elias@cabureweb.com>
2018-01-17 12:26:23 -0300

    Agrega las librerias en frontend, backend y login

--- includes/librerias.php
+++ includes/librerias.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) exit;
 /*
  * @const WPBASE_GOOGLEMAPS_API_KEY string
  */
-function registrarLibreriasFrontend() {
+function registrarLibrerias() {
     // Fuentes
     wp_register_style('google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700', array(), null);
 
@@ -21,27 +21,10 @@ function registrarLibreriasFrontend() {
     // Lightbox - https://github.com/lokesh/lightbox2/
     wp_register_script('lightbox', ASSETS_URI_JS . 'vendor/lightbox.min.js', array('jquery'), '2.8.2', true);
 
-    // Google Maps - https://developers.google.com/maps/?hl=es-419
-    // Llamar a 'gmaps' en el enqueue.
-    $args = array(
-        'key' => WPBASE_GOOGLEMAPS_API_KEY,
-        'callback' => 'initMap',
-    );
-    wp_register_script('mapa', ASSETS_URI_JS . 'mapa.js', array(), VERSION_THEME, true);
-    wp_register_script('gmaps', 'https://maps.googleapis.com/maps/api/js?' . http_build_query($args), array('jquery', 'mapa'), null, true);
-    wp_localize_script('gmaps', 'WPURLS', array(
-        'home_url'  => HOME_URL,
-        'theme_uri' => THEME_URI,
-    ));
-}
-add_action('wp_enqueue_scripts', 'registrarLibreriasFrontend', 1);
-
-function registrarLibreriasBackend() {
+    // Backend
     wp_register_style('backend', ASSETS_URI_CSS . 'backend.min.css', array(), VERSION_THEME);
-}
-add_action('admin_enqueue_scripts', 'registrarLibreriasBackend', 1);
 
-function registrarLibreriasLogin() {
+    // Login
     wp_register_style('custom-login', ASSETS_URI_CSS . 'login.min.css');
 }
-add_action('login_enqueue_scripts', 'registrarLibreriasLogin', 1);
+add_action('init', 'registrarLibrerias');
