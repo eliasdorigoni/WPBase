fb60a51cf6db919eb7b6715fd97252dd91c5d498
Elias Dorigoni <elias@cabureweb.com>
2018-02-15 15:47:48 -0300

    Elimina filtro no usado

--- functions.php
+++ functions.php
@@ -20,7 +20,6 @@ get_template_part('includes/woocommerce/init');
 
 // Agrega el slug del post y el tipo + slug del post al output de body_class()
 add_filter('body_class', 'theme_agregarNombreEnBody');
-add_filter('post_thumbnail_html', 'ampliarPostThumbnail', 10, 5);
 
 // Configura el enlace a la web en login.
 add_filter('login_headertitle', 'get_bloginfo');
